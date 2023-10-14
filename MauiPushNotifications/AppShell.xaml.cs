2b3a3b10155a25c4f8c6666b21c4d26d998856ef
Dmitry Ordenov <dordenov@artec3d.com>
2023-10-14 12:43:49 +0200

    init project

---
+++ MauiPushNotifications/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiPushNotifications;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MauiPushNotifications/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiPushNotifications;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
